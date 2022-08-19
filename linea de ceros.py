ba06789e2de50c47cc18739c5934d0b7e89ab741
robinsontru <109121630+robinsontru@users.noreply.github.com>
2022-08-19 16:59:51 -0500

    Add files via upload

---
+++ linea de ceros.py
@@ -0,0 +1,18 @@
+matriz=int(input("ingrese que numero quiere q se muestre en la linea"))
+
+for i in range (1,11):
+    for j in range (1,10):
+        if (j==i):
+             print(matriz,end=" ")              
+        else: 
+           print(f"{j}",end=" ")
+    print(" ")
+
+###########
+    for i in range (1,11):
+     for j in range (1,10):
+        if (j==i):
+             print(matriz,end=" ")              
+        else: 
+           print(f"{j}",end=" ")
+    print(" ")
